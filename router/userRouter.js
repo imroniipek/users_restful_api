fd51736d435531e1c900337e837005914e7d08ff
imroniipek <roniipek8@gmail.com>
2026-01-30 17:45:59 +0300

    Router ve http dosyaları güncellendi, db bağlantısı düzenlendi

--- router/userRouter.js
+++ router/userRouter.js
@@ -32,4 +32,57 @@ router.post('/ekle', async (req, res) => {
     }
 });
 
+router.delete('/:id',async (req,res)=>{
+
+    try{
+
+         console.log("Silinecek id:"+req.params.id);
+
+         const sonuc=await userDb.findByIdAndDelete(req.params.id)
+
+
+         if(sonuc)
+            {
+              return res.status(201).json({
+                mesaj:"İşlem Basarili",
+             });
+            }
+            else
+            {
+
+            }
+
+    }
+    catch(error)
+    {
+        res.status(404).send("Hata Oluştu");
+    }
+});
+
+router.patch('/:id', async (req, res) => {
+    try {
+        
+        const guncelKullanici = await userDb.findByIdAndUpdate(
+            req.params.id, 
+            req.body, 
+            { new: true, runValidators: true } 
+        ); 
+
+        if (guncelKullanici) {
+            return res.status(200).json(guncelKullanici); 
+        } else {
+            return res.status(404).json({
+                mesaj: "Güncellenecek kullanıcı bulunamadı."
+            });
+        }
+    } catch (error) {
+        console.error("Güncelleme hatası:", error);
+        res.status(500).json({
+            hata: "Sunucu hatası oluştu",
+            detay: error.message
+        });
+    }
+});
+
+
 module.exports = router;
